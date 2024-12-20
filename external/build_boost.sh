796a9460a3671e9937bbc6335068f9040b8b5163
Franz R. Sattler <sattler@thphys.uni-heidelberg.de>
2024-12-20 13:10:15 +0100

    fix: deal.ii boost dependency was missing (iostreams and thread), added them to the boost setup.
    Signed-off-by: Franz R. Sattler <sattler@thphys.uni-heidelberg.de>

--- external/build_boost.sh
+++ external/build_boost.sh
@@ -9,16 +9,16 @@ source ./build_scripts/setup_folders.sh
 
 cd $BUILD_PATH
 
-COMPILER_CXX=`cmake -E environment | grep "CXX" | awk -F'=' '{print $3}'`
+COMPILER_CXX=$(cmake -E environment | grep "CXX" | awk -F'=' '{print $3}')
 if [ -z "$COMPILER_CXX" ]; then
-    COMPILER_CXX=`which g++`
-    echo "No compiler for boost build specified, choosing ${COMPILER_CXX}";
+  COMPILER_CXX=$(which g++)
+  echo "No compiler for boost build specified, choosing ${COMPILER_CXX}"
 fi
 
-COMPILER=`cmake -E environment | grep "CC" | awk -F'=' '{print $2}'`
+COMPILER=$(cmake -E environment | grep "CC" | awk -F'=' '{print $2}')
 if [ -z "$COMPILER" ]; then
-    COMPILER=`which gcc`
-    echo "No compiler for boost build specified, choosing ${COMPILER}";
+  COMPILER=$(which gcc)
+  echo "No compiler for boost build specified, choosing ${COMPILER}"
 fi
 
 echo $COMPILER
@@ -26,12 +26,14 @@ cd $SOURCE_PATH
 
 ./bootstrap.sh --prefix=${INSTALL_PATH}
 ./b2 --build-dir=${BUILD_PATH} \
-     --prefix=${INSTALL_PATH} \
-     --with-json \
-     --with-math \
-     --with-serialization \
-     --with-system \
-     --with-headers \
-     -j ${THREADS} \
-     cxxflags="-std=c++20" \
-     install | tee $MAKE_LOG_FILE
+  --prefix=${INSTALL_PATH} \
+  --with-headers \
+  --with-iostreams \
+  --with-json \
+  --with-math \
+  --with-serialization \
+  --with-system \
+  --with-thread \
+  -j ${THREADS} \
+  cxxflags="-std=c++20" \
+  install | tee $MAKE_LOG_FILE
