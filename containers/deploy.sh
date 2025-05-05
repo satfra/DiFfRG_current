10a4e0a24ef1d0cb103470d5c3d0e9e468c50b78
Franz R. Sattler <sattler@thphys.uni-heidelberg.de>
2025-05-05 20:37:35 +0200

    Signed-off-by: Franz R. Sattler <sattler@thphys.uni-heidelberg.de>

--- containers/deploy.sh
+++ containers/deploy.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/satfra/DiFfRG.git --depth 1 --branch Implement-Kokkos
 mkdir -p build
 cd build
-cmake ../DiFfRG -DCMAKE_BUILD_TYPE=Release
+cmake ../DiFfRG -DCMAKE_BUILD_TYPE=Release -DKokkos_ARCH_LIST=Kokkos_ARCH_ADA89
 make -j8
--- containers/test_all.sh
+++ containers/test_all.sh
@@ -4,11 +4,16 @@
 # This script tries to build the DiFfRG docker images for all setups in the Base/ directory.
 
 # get all files in Base/
-images=$(ls Base/)
+base_images=$(ls Base/)
+# get all files in MPI+OpenMP/
+mpi_openmp_images=$(ls MPI+OpenMP/)
+# get all files in MPI+OpenMP+CUDA/
+mpi_openmp_cuda_images=$(ls MPI+OpenMP+CUDA/)
+mkdir -p logs
 
-for image in $images; do
+for image in $mpi_openmp_cuda_images; do
     echo "   Building DiFfRG docker image for $image..."
-    docker buildx build -t diffrg-$image -f Base/$image . --no-cache --progress=plain &>$image.log
+    docker buildx build -t diffrg-$image -f MPI+OpenMP+CUDA/$image . --no-cache --progress=plain &>logs/$image.log
     if [ $? -ne 0 ]; then
         echo "   Error building $image. Check the log file $image.log for details."
     else
@@ -17,5 +22,4 @@ for image in $images; do
     echo "   Cleaning up..."
     # remove the image from the local docker registry
     docker rmi diffrg-$image
-
 done
