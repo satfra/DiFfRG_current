9374a381cd7ed7545f9c2048d561ac4f708ce8c3
Keiwan Jamaly <keiwan@jamaly.de>
2026-05-25 16:21:17 +0200

    adjusted simulation parameters

--- DiFfRG/tests/discretization/FV/regression/quark_meson_kt_regression.cc
+++ DiFfRG/tests/discretization/FV/regression/quark_meson_kt_regression.cc
@@ -39,7 +39,7 @@ namespace
   constexpr double sigma_min = 0.0;
   constexpr double sigma_max_over_lambda = 1.5;
   constexpr double sigma_max = sigma_max_over_lambda * lambda_uv;
-  constexpr double delta_sigma = 3.0e-3; // N = (sigma_max - sigma_min)/delta_sigma = 300 cells
+  constexpr double delta_sigma = 1.0e-3; // N = (sigma_max - sigma_min)/delta_sigma = 900 cells
   constexpr std::size_t n_cells = static_cast<std::size_t>((sigma_max - sigma_min) / delta_sigma + 0.5);
   constexpr double grid_tol = 1.0e-12;
 
@@ -384,7 +384,7 @@ namespace
   {
     return json::value(
         {{"physical", {{"Lambda", lambda_uv}, {"cSigma", PaperLitimParameters::cSigma}}},
-         {"integration", {{"x_order", 32}, {"x_extent_tolerance", 1.0e-4}, {"jacobian_quadrature_factor", 0.5}}},
+         {"integration", {{"x_order", 100}, {"x_extent_tolerance", 1.0e-4}, {"jacobian_quadrature_factor", 0.5}}},
          {"discretization",
           {{"fe_order", 0},
            {"threads", 16},
@@ -407,8 +407,8 @@ namespace
             {{"dt", 1.0e-9},
              {"minimal_dt", 1.0e-15},
              {"maximal_dt", 1.0e-5},
-             {"abs_tol", 1.0e-10},
-             {"rel_tol", 1.0e-10},
+             {"abs_tol", 1.0e-12},
+             {"rel_tol", 1.0e-12},
              {"max_steps", 20000000}}}}},
          {"output",
           {{"verbosity", 1},
