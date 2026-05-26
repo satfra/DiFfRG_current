5f296928eec0c76d7aa03f053cfee4e2f2490471
Keiwan Jamaly <keiwan@jamaly.de>
2026-05-27 00:52:02 +0200

    Use Boost zeta in loop integral tests

--- DiFfRG/tests/physics/loop_integrals.cc
+++ DiFfRG/tests/physics/loop_integrals.cc
@@ -5,6 +5,8 @@
 #include <DiFfRG/common/polynomials.hh>
 #include <DiFfRG/physics/loop_integrals.hh>
 
+#include <boost/math/special_functions/zeta.hpp>
+
 #include <cmath>
 
 using namespace DiFfRG;
@@ -221,7 +223,7 @@ TEMPLATE_TEST_CASE_SIG("Test momentum integrals with matsubara sum",
   coeff_integrand[ds - 1] = 1.;
   square_poly *= Polynomial(coeff_integrand);
   const double reference_integral = S_ds(ds) * square_poly.integral(0., q_extent) / powr<ds>(2. * M_PI) *
-                                    (1. + 2. * std::riemann_zeta(zeta_val)) * T_val;
+                                    (1. + 2. * boost::math::zeta(zeta_val)) * T_val;
 
   const QGauss<1> x_quadrature(64);
   const QGauss<1> q0_quadrature(32);
@@ -273,7 +275,7 @@ TEMPLATE_TEST_CASE_SIG("Test momentum integrals with matsubara sum and angle",
   coeff_integrand[ds - 1] = 1.;
   square_poly *= Polynomial(coeff_integrand);
   const double reference_integral = S_ds(ds) / 2. * square_poly.integral(0., q_extent) / powr<ds>(2. * M_PI) *
-                                    cos_poly.integral(-1., 1.) * (1. + 2. * std::riemann_zeta(zeta_val)) * T_val;
+                                    cos_poly.integral(-1., 1.) * (1. + 2. * boost::math::zeta(zeta_val)) * T_val;
 
   const QGauss<1> x_quadrature(64);
   const QGauss<1> q0_quadrature(32);
@@ -486,7 +488,7 @@ integration][adaptive integration]", ((int dim), dim), (2), (3), (4))
   coeff_integrand[ds - 1] = 1.;
   square_poly *= Polynomial(coeff_integrand);
   const double reference_integral = S_ds(ds) * square_poly.integral(0., q_extent) / powr<ds>(2. * M_PI) * (1. + 2. *
-std::riemann_zeta(zeta_val)) * T_val;
+boost::math::zeta(zeta_val)) * T_val;
 
   double integral = AdaptiveLoopIntegrals::spatial_integrate_and_sum<double, dim>(
       [&](const auto &x, const auto &x0) { return is_close(x0, 0.) ? poly(x) : poly(x) / std::pow(std::abs(x0) / (2. *
@@ -532,7 +534,7 @@ angle integration][adaptive integration]", ((int dim), dim), (2), (3), (4))
   square_poly *= Polynomial(coeff_integrand);
   const double reference_integral =
       S_ds(ds) / 2. * square_poly.integral(0., q_extent) / powr<ds>(2. * M_PI) * cos_poly.integral(-1., 1.) * (1. + 2. *
-std::riemann_zeta(zeta_val)) * T_val;
+boost::math::zeta(zeta_val)) * T_val;
 
   double integral = AdaptiveLoopIntegrals::spatial_angle_integrate_and_sum<double, dim>(
       [&](const auto &x, const auto &cos, const auto &x0) {
